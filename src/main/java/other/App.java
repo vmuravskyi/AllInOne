70bff4b39fbaa93e05e10780a8c5b5984f3d427a
Volodymyr Muravskyi <v.muravskyi@gmail.com>
2021-08-20 01:00:48 +0300

    selTest

--- src/main/java/other/App.java
+++ src/main/java/other/App.java
@@ -6,20 +6,23 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(": ");
-        int x = sc.nextInt();
-        System.out.println(": ");
-        int y = sc.nextInt();
-        int choice = 0;
-        do {
-            System.out.println("1. Show tasks");
-            System.out.println("2. ");
-            System.out.println("3. ");
-            System.out.println("4. ");
-            System.out.println("0. ");
-            choice = sc.nextInt();
-        } while (choice != 0);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(": ");
+//        int x = sc.nextInt();
+//        System.out.println(": ");
+//        int y = sc.nextInt();
+//        int choice = 0;
+//        do {
+//            System.out.println("1. Show tasks");
+//            System.out.println("2. ");
+//            System.out.println("3. ");
+//            System.out.println("4. ");
+//            System.out.println("0. ");
+//            choice = sc.nextInt();
+//        } while (choice != 0);
+
+
+
 
     }
 }
