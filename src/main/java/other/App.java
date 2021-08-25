3ab6461f1d671c076fd4ae2806fe047ede3de56d
Volodymyr Muravskyi <v.muravskyi@gmail.com>
2021-08-26 00:32:52 +0300

    semaphore

--- src/main/java/other/App.java
+++ src/main/java/other/App.java
@@ -1,9 +1,8 @@
 package other;
-import static java.lang.System.*;
 
 
 public class App {
     public static void main(String[] args) {
-        out.println("Hello");
+        System.out.println("Hello");
     }
 }
