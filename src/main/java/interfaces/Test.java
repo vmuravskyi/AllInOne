4394786e79d1a55e727b8b1bd4a1790a926e4b35
vmuravskyi <v.muravskyi@gmail.com>
2021-10-12 17:08:39 +0300

    save

--- src/main/java/interfaces/Test.java
+++ src/main/java/interfaces/Test.java
@@ -1,4 +1,4 @@
-package Interfaces;
+package interfaces;
 
 public class Test {
     public static void main(String[] args) {
---
+++ src/main/java/javaFromEpam/lecture02/Test.java
@@ -0,0 +1,21 @@
+package javaFromEpam.lecture02;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        for (int i = 1; i <= 9; i++) {
+            a = i;
+            for (int j = 1; j <= 9; j++) {
+                b = j;
+                c = i * j;
+                System.out.printf(a + "*" + b + " = " + c + " ");
+            }
+            System.out.println();
+        }
+    }
+}
