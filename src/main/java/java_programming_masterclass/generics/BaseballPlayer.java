a578005948a17e0d611ee4ea8acd43bbe28875e2
v.muravskyi <v.muravskyi@gmail.com>
2022-05-21 18:58:19 +0300

    save

---
+++ src/main/java/java_programming_masterclass/generics/BaseballPlayer.java
@@ -0,0 +1,10 @@
+package java_programming_masterclass.generics;
+
+public class BaseballPlayer extends Player {
+
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+
+
+}
---
+++ src/main/java/java_programming_masterclass/generics/Main.java
@@ -0,0 +1,28 @@
+package java_programming_masterclass.generics;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+//        adelaideCrows.addPlayer(pat);
+//        adelaideCrows.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("this should not work");
+        brokenTeam.addPlayer(beckham);
+
+        System.out.println(baseballTeam.numPlayers());
+        System.out.println(brokenTeam.numPlayers());
+    }
+
+}
