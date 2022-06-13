2efb3641c84befa5e6c8abc0190ad05c122f9a0f
v.muravskyi <v.muravskyi@gmail.com>
2022-06-13 13:32:54 +0300

    save

---
+++ src/main/java/reflections_and_annotation/section05_hibarnate_like_orm/Launcher.java
@@ -0,0 +1,15 @@
+//package reflections_and_annotation.section05_hibarnate_like_orm;
+//
+//import java.sql.SQLException;
+//
+//import org.h2.tools.Server;
+//
+//public class Launcher {
+//
+//	public static void main(String[] args) throws SQLException {
+//
+//		Server.main();
+//
+//	}
+//
+//}
