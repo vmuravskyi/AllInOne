e2b89eb0e2aa20434afb2a01a6a51ebd3246f2e2
v.muravskyi <v.muravskyi@gmail.com>
2022-09-14 01:14:50 +0300

    Path & Files

---
+++ src/main/java/java_black_belt/section09_IO_NIO_Streams/lesson103_Path_Files/PathAndFilesEx2.java
@@ -0,0 +1,28 @@
+package java_black_belt.section09_IO_NIO_Streams.lesson103_Path_Files;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathAndFilesEx2 {
+
+    public static void main(String[] args) throws IOException {
+
+        Path filePath = Paths.get("PathAndFilesEx1.txt");
+        Path directoryPath = Paths.get("C:\\Users\\vmura\\OneDrive\\Desktop\\TestFolder");
+        Path directoryBPath = Paths.get("C:\\Users\\vmura\\OneDrive\\Desktop\\TestFolder2");
+
+//        Files.copy(filePath, directoryPath.resolve(filePath), StandardCopyOption.REPLACE_EXISTING);
+//        Files.copy(filePath, directoryPath.resolve("myCustomFilename.txt"));
+//        Files.copy(directoryBPath, directoryPath.resolve("B")); // copy with replacing the name of the folder, will copy without its content
+
+//        Files.move(filePath, directoryPath.resolve("testMovedFile.txt"));
+
+//        Files.move(Paths.get("test1123123123121.txt"), Paths.get("test1123123123121Renamed.txt")); // rename file
+
+        Files.deleteIfExists(directoryBPath);
+
+    }
+
+}
---
+++ src/main/java/java_black_belt/section09_IO_NIO_Streams/lesson103_Path_Files/PathAndFilesEx3.java
@@ -0,0 +1,25 @@
+package java_black_belt.section09_IO_NIO_Streams.lesson103_Path_Files;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class PathAndFilesEx3 {
+
+    public static void main(String[] args) throws IOException {
+
+        Path filePath = Paths.get("PathAndFilesEx3.txt");
+        if (!Files.exists(filePath)) {
+            Files.createFile(filePath);
+        }
+        String dialog = "-Hello\n-Hello\n-How are you doing?\n-Great! How about you?\n-I'm doing well, thanks!";
+        Files.write(filePath, dialog.getBytes());
+
+        List<String> lines = Files.readAllLines(filePath);
+        lines.forEach(System.out::println);
+
+    }
+
+}
